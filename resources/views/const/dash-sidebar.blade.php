9339a95c54915a7f480c1ee169f3692b46d5757e
tripletens <tripletens.kc@gmail.com>
2022-01-17 09:24:27 +0100

    added the new logo to the dashboard

--- resources/views/const/dash-sidebar.blade.php
+++ resources/views/const/dash-sidebar.blade.php
@@ -2,13 +2,9 @@
 <ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
 
 <!-- Sidebar - Brand -->
+
 <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('landing-page')}}">
-    <img src="{{asset('images/Kanselor_transparentwhite.png')}}" style="height:100px;width:auto;padding:20px;" alt="Kaneslor Logo">
-    <!-- <div class="sidebar-brand-icon rotate-n-15">
-        <i class="fas fa-laugh-wink"></i>
-    </div> -->
-    
-    <!-- <div class="sidebar-brand-text mx-3">{{ config('app.name', 'App') }} </div> -->
+    <img src="{{asset('images/kiyix_logo_white.png')}}" style="height:100px;width:auto;padding:20px;" alt="Kaneslor Logo">
 </a>
 
 <!-- Divider -->
